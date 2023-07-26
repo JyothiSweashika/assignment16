d1d27ebb8b094a12be1ace1a6852a43fe9d835af
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-07-26 15:49:47 +0530

    assignment16

---
+++ Bankaccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    internal class Bankaccount
+    {
+        private static long accountCounter = 256754522565;
+        public long AccountNumber { get; }
+        public string AccountHolderName { get; }
+        private double Balance { get; set; }
+
+        public Bankaccount(string accountHolderName)
+        {
+            AccountNumber = GenerateAccountNumber();
+            AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+
+        private static long GenerateAccountNumber()
+        {
+            return ++accountCounter;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be greater than zero.");
+            }
+
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawal amount must be greater than zero.");
+            }
+
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+
+            Balance -= amount;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
